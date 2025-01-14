5d566a834ea42176113ed52c1e75efb1d82c3e91
Grace Lieu <lieuyeuhuey0909@gmail.com>
2025-01-14 16:17:51 +0800

    Fourth Commit

--- app/com.marketplace/src/main/java/com/marketplace/MarketplaceProjectApplication.java
+++ app/com.marketplace/src/main/java/com/marketplace/MarketplaceProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketplaceProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceProjectApplication.class, args);
 	}
-
 }
